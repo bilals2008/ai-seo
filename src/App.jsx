1e9a273c5c3743b879922c5b7794e3280428e783
bilals2008 <wbiz2939@gmail.com>
2025-09-19 08:01:22 +0500

    adding font

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 // File: src/App.jsx
 import { Button } from "@/components/ui/button";
-
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
