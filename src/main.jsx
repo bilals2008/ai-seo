6025fe5aa8ec3047deadd9222719bb5fdde4ac1a
bilals2008 <wbiz2939@gmail.com>
2025-09-24 12:55:29 +0500

    added react router

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-
+const routes = create
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
