4ce3b466e05fe8503e287e956f602252a273fcae
bilals2008 <wbiz2939@gmail.com>
2025-09-24 13:12:39 +0500

    feat(Home): add Navbar component to the Home page

--- src/ai-seo-2/Home.jsx
+++ src/ai-seo-2/Home.jsx
@@ -1,11 +1,12 @@
 // File: src/ai-seo-2/Home.jsx
 import React from "react";
+import Navbar from "../components/Navbar/Navbar";
 
 function Home() {
   return (
     <div className="bg-[#05020a] text-white h-auto pb-10">
       <div className="max-w-6xl mx-auto ">
-        
+        <Navbar/> 
       </div>
     </div>
   );
