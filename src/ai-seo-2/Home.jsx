ab6bcb1aeb680262cb341b5993f2e9e864b15441
bilals2008 <wbiz2939@gmail.com>
2025-09-24 13:11:10 +0500

    add basic style for home website

---
+++ src/ai-seo-2/Home.jsx
@@ -0,0 +1,14 @@
+// File: src/ai-seo-2/Home.jsx
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-[#05020a] text-white h-auto pb-10">
+      <div className="max-w-6xl mx-auto ">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
