d4236a3b2c0244ddbf2f42fb7dcf8880353e2a71
bilals2008 <wbiz2939@gmail.com>
2025-09-24 13:09:33 +0500

    add frist websie and second website routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,24 @@
 // File: src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App'
-const routes = create
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ai-seo-2/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <App />, //first website
+  },
+  {
+    path: "/ai-seo-2", 
+    element: <Home />, //second website
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
