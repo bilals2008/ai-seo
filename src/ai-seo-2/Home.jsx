50ab3039d3a586d9c19cce99b34bef37f767d3b4
bilals2008 <wbiz2939@gmail.com>
2025-09-26 21:49:55 +0500

    add lenis for smooth sccrolling in ai-seo-2  page

--- src/ai-seo-2/Home.jsx
+++ src/ai-seo-2/Home.jsx
@@ -5,22 +5,42 @@ import Hero from "./Hero/Hero";
 import Features from "./features/Features";
 import Client from "./Client/Client";
 import Pricing from "./Pricing/Pricing";
-
+import { ReactLenis, useLenis } from "lenis/react";
 function Home() {
+  const lenis = useLenis((lenis) => {
+    // called every scroll
+    console.log(lenis);
+  });
   return (
-    <div className="bg-[#05020a] text-white h-auto pb-10 overflow-x-hidden">
-      <div className="max-w-6xl mx-auto relative pt-2">
-        <div className="w-11/12 border border-white/30 rounded-md mx-auto space-y-10">
-          <Navbar seprator={false} />
+    <>
+      <ReactLenis
+        root
+        options={{
+          duration: 1.2,
+          lerp: 0.08,
+          smoothWheel: true,
+          smoothTouch: true,
+          infinite: false,
+          gestureDirection: "vertical",
+          orientation: "vertical",
+          normalizeWheel: true,
+        }}
+      >
+        <div className="bg-[#05020a] text-white h-auto pb-10 overflow-x-hidden">
+          <div className="max-w-6xl mx-auto relative pt-2">
+            <div className="w-11/12 border border-white/30 rounded-md mx-auto space-y-10">
+              <Navbar seprator={false} />
+            </div>
+          </div>
+          <Hero />
+          <Features />
+          <div className="py-10">
+            <Client />
+          </div>
+          <Pricing />
         </div>
-      </div>
-      <Hero />
-      <Features />
-      <div className="py-10">
-        <Client />
-      </div>
-      <Pricing />
-    </div>
+      </ReactLenis>
+    </>
   );
 }
 
