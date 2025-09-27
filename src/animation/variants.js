6a058825c3b7840982d7ef99e13db8bbfe8ca57f
bilals2008 <wbiz2939@gmail.com>
2025-09-27 16:13:48 +0500

    feat: add motion library and implement animations in Navbar component

--- src/animation/variants.js
+++ src/animation/variants.js
@@ -1,1 +1,14 @@
 // File: src/animation/variants.js
+export const containerStagger = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+      staggerChildren: 0.3,
+    },
+  },
+};
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,28 @@
-// File: src/components/Navbar/Navbar.jsx
-import React from 'react'
-import Logo from './Logo'
-import NavElements from './NavElements'
-import NavBtton from './NavBtton'
-import { Separator } from "@/components/ui/separator"
+import React from "react";
+import { motion } from "motion/react";
+import Logo from "./Logo";
+import NavElements from "./NavElements";
+import NavBtton from "./NavBtton";
+import { Separator } from "@/components/ui/separator";
+import { containerStagger } from "../../animation/variants";
+
 function Navbar({ className, seprator, border }) {
   return (
-    <div>
-      <div className="flex justify-between md:justify-evenly items-center p-2 px-5">
+    <motion.div variants={containerStagger} initial="hidden" animate="visible">
+      <motion.div
+        className="flex justify-between md:justify-evenly items-center p-2 px-5"
+        variants={{
+          hidden: { y: -20, opacity: 0 },
+          visible: { y: 0, opacity: 1 },
+        }}
+      >
         <Logo />
         <NavElements className={className} />
         <NavBtton border={border} />
-      </div>
+      </motion.div>
       {seprator && <Separator className="bg-[#262628] mt-1" />}
-    </div>
+    </motion.div>
   );
 }
 
-export default Navbar
+export default Navbar;
